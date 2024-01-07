9637a97791ce7084630f7de79897c89b3a1a1286
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-07 15:33:06 -0800

    explore binary trees

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,7 +12,12 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 	if (node->n == node->parent->left->n)
+	{
+		if (!node->parent->right)
+			return (NULL);
 		return (node->parent->right);
-	else
-		return (node->parent->left);
+	}
+	else if (!node->parent->left)
+		return (NULL);
+	return (node->parent->left);
 }
